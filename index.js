f504f2c7b315d290bd30ee6e828c2d0b1c4fed9e
Mark Bailey <markbailey78@yahoo.co.uk>
2021-09-28 21:59:30 +0100

    Added config options

--- index.js
+++ index.js
@@ -1,104 +1,140 @@
 module.exports = {
-  root: true,
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
     ecmaFeatures: { jsx: true },
-    project: './tsconfig.json',
-    tsconfigRootDir: __dirname
   },
-  ignorePatterns: ['**/*.js'],
+  ignorePatterns: ['**/*.js', '**/*.d.ts'],
   settings: { react: { version: 'detect' } },
   env: {
     browser: true,
     es2020: true,
     jest: true,
-    node: true
+    node: true,
   },
-  extends: ['airbnb', 'airbnb-typescript', 'plugin:prettier/recommended'],
-  plugins: ['react', 'react-hooks', '@typescript-eslint'],
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking'
+  ],
+  plugins: [
+    'react', 
+    'react-hooks', 
+    '@typescript-eslint', 
+    'import-newlines'
+  ],
   globals: {
     NodeJS: true,
     JSX: true,
-    otherGlobal: true
   },
   rules: {
-    'comma-dangle': ['error', 'always-multiline'],
-    curly: ['error', 'multi', 'consistent'],
-    'import/prefer-default-export': 'off',
-    'linebreak-style': ['error', 'unix'],
-    'lines-between-class-members': 'off',
-    'max-len': 'off',
-    'max-statements-per-line': ['error', { max: 1 }],
-    'no-bitwise': 'off',
-    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
-    'no-restricted-syntax': ['error', 'ForInStatement', 'LabeledStatement', 'WithStatement'],
-    'object-curly-newline': [
-      'error',
-      {
-        ObjectExpression: { multiline: true, minProperties: 3 },
-        ObjectPattern: { multiline: true, minProperties: 3 },
-        ImportDeclaration: { multiline: true, minProperties: 3 },
-        ExportDeclaration: { multiline: true, minProperties: 3 },
-        consistent: true
-      }
-    ],
-
-    // PRETTIER
-    'prettier/prettier': [
-      'error',
-      {
-        eslintIntegration: true,
-        trailingComma: 'none',
-        tabWidth: 2,
-        useTabs: false,
-        semi: true,
-        singleQuote: true,
-        bracketSpacing: true,
-        arrowParens: 'always',
-        endOfLine: 'lf',
-        proseWrap: 'always',
-        htmlWhitespaceSensitivity: 'strict',
-        requirePragma: false
-      }
-    ],
-
-    // REACT
-    'react/jsx-filename-extension': ['error', { extensions: ['.jsx', '.tsx'] }],
-    'react/jsx-props-no-spreading': [
-      'error',
-      {
-        html: 'enforce',
-        custom: 'ignore'
-      }
-    ],
-    'react/jsx-no-useless-fragment': ['error'],
-
     // TYPESCRIPT
+    '@typescript-eslint/comma-dangle': 'off',
     '@typescript-eslint/dot-notation': [
       'error',
       {
         allowPrivateClassPropertyAccess: true,
         allowProtectedClassPropertyAccess: true
-      }
+      },
     ],
+    '@typescript-eslint/indent': 'off',
     '@typescript-eslint/lines-between-class-members': [
       'error',
       'always',
       {
         exceptAfterSingleLine: true,
         exceptAfterOverload: true
+      },
+    ],
+
+    // BASE ESLINT
+    'brace-style': 'error',
+    'class-methods-use-this': 'off',
+    'comma-dangle': ['error', 'always-multiline'],
+    'consistent-return': 'warn',
+    'curly': ['error', 'multi', 'consistent'],
+    'indent': 'error',
+    'linebreak-style': ['error', process.platform === 'win32' ? 'windows' : 'unix'],
+    'lines-between-class-members': 'off',
+    'max-len': 'off',
+    'max-statements-per-line': ['error', { max: 1 }],
+    'no-bitwise': 'off',
+    'no-plusplus': 'off',
+    'no-param-reassign': ['error', { props: false }],
+    'no-restricted-properties': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'ForInStatement',
+      'LabeledStatement',
+      'WithStatement',
+    ],
+    'nonblock-statement-body-position': ['error', 'below'],
+    'object-curly-newline': [
+      'error', 
+      {
+        'ObjectExpression': { 'multiline': true, 'minProperties': 3 },
+        'ObjectPattern': { 'multiline': true },
+        'ImportDeclaration':  { 'multiline': true, 'minProperties': 3 },
+        'ExportDeclaration': { 'multiline': true, 'minProperties': 3 }
       }
-    ]
+    ],
+    'object-property-newline': 'error',
+    'padding-line-between-statements': [
+      'error',
+      { 
+        blankLine: 'always', 
+        prev: ['block-like', 'const', 'directive', 'let'], 
+        next: '*' 
+      },
+      { 
+        blankLine: 'any', 
+        prev: ['const', 'let'], 
+        next: ['const', 'let'] 
+      },
+    ],
+    'sort-imports': ['error'],
+
+    // PLUGINS
+    'import/extensions': 'off',
+    'import-newlines/enforce': [
+      'error', 
+      {
+        items: 1,
+        // 'max-len': 100,
+        // semi: false
+      }
+    ],
+
+    // JSX-A11Y
+    'jsx-a11y/label-has-associated-control': [ 
+      2, 
+      {
+        assert: 'either',
+        depth: 3,
+      }
+    ],
+
+    // REACT
+    'react/jsx-filename-extension': ['error', { extensions: ['.jsx', '.tsx'] }],
+    'react/jsx-props-no-spreading':[
+      'error', 
+      {
+        html: 'enforce',
+        custom: 'ignore',
+      }
+    ],
+    'react/jsx-no-useless-fragment': 'error',
   },
   overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
+    { 
+      files: ['*.ts', '*.tsx'], 
       excludedFiles: ['*.d.ts'],
       rules: {
-        'react/require-default-props': 'off'
-      }
-    }
-  ]
+        'react/require-default-props': 'off',
+      } 
+    },
+  ],
 };
